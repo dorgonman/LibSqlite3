e6758ae75e5de6a7dacdc690b47e3e4d92082f49
dorgon.chang <dorgonman@hotmail.com>
2017-12-24 16:36:08 +0800

    add vs 2017

---
+++ project_generator/windows/win32/vs2017/build_all.sh
@@ -0,0 +1,5 @@
+set -e
+echo ====================Start Debug Build====================================
+./build_debug.sh
+echo ====================Start Release Build====================================
+./build_release.sh
